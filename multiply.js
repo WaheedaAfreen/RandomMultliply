cb452d542b42656984e3192d4f5c3bdea5907926
Afreen <waheeda15-11581@diu.edu.bd>
2023-05-04 17:03:55 +0600

    Update valid number or Invalid number  form reset

--- multiply.js
+++ multiply.js
@@ -1,16 +1,19 @@
+
 const scoreEl = document.getElementById("score");
 const questionEl = document.getElementById("question");
 const form = document.getElementById("form");
+const input = document.getElementById("input");
 
 let score = 0;
-let num1, num2; // Declare num1 and num2 globally
+let num1, num2;
 
 function generateQuestion() {
-    num1 = Math.floor(Math.random() * 10) + 1;
-    num2 = Math.floor(Math.random() * 10) + 1;
+    num1 = Math.floor(Math.random() * 10) ;
+    num2 = Math.floor(Math.random() * 10) ;
     questionEl.innerText =`What is ${num1} multiplied by ${num2}?` ;
 }
 function updateScore(isCorrect) {
+
     if (isCorrect) {
         score++;
         alert("Correct!");
@@ -18,17 +21,43 @@ function updateScore(isCorrect) {
         score--;
         alert("Incorrect. Try again.");
     }
-    scoreEl.innerText =` Score: ${score}`;
+
+    scoreEl.innerText =`Score: ${score}`;
     generateQuestion();
 }
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    const userAnswer = parseInt(document.getElementById("input").value);
-    const correctAnswer = num1 * num2;
-    const isCorrect = userAnswer === correctAnswer;
-    updateScore(isCorrect);
-});
 
-// Initial question generation
-generateQuestion();
+
+        if (isNumberValid(input.value)) {
+            event.preventDefault();
+            alert("valid number.");
+        }
+
+        if (!isNumberValid(input.value)) {
+            event.preventDefault();
+            alert("Invalid number  Please enter a valid number.");
+        }
+
+        const userAnswer = parseInt(document.getElementById("input").value);
+        const correctAnswer = num1 * num2;
+        const isCorrect = userAnswer === correctAnswer;
+        updateScore(isCorrect);
+        form.reset();
+
+
+        function isNumberValid(value) {
+            return !isNaN(parseFloat(value)) && isFinite(value);
+        }
+
+    });
+
+    generateQuestion();
+
+
+
+
+
+
+
